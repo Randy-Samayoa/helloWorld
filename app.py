0b06ccaba77dfc9b10f61bcfe15ecf4a8a4c3bf7
Randy <rsamayoa@umd.edu>
2024-02-14 23:43:24 -0500

    Committing my app.py code with the new app route of my about.html page

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! Randy Samayoa Robert Smith. I am adding my first code change'
+    return 'Hello World! Randy Samayoa from Robert H. Smith. I am adding my first code change'
 
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
